984504f6b24c2a6008c54970be03dc69b2cf586c
Jateeeen <75193966+Jateeeen@users.noreply.github.com>
2023-05-23 18:44:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 0000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('main');
+});
+
+app.get('/dchsuejbehhshjdbbshhsbsksihenjehehshbebehshebhehsjenshjsjskaksndnhsiskaknsnsisjjsjsjsksibsnshjsjsbshsusiksnhshsiskjsnbshsiaoaksnsjjsjakkskksnjsjjsoskksknshshsiiskskksjenendhdhuosowjebdhhaokskejdhhsisiakskejwkwopwisjdbbehsiskakdnjdjsiwjejdjdbhaiososoejndhsuaoosjdjdhdhiwoaodhdbjsiaosodjjdndksoaidjdjdjsoosodjdhskaosodidjdjjdjsiosidfhhdjsjsksodidifjjdjsjwksdjjffjjajhdhdhjsksisisjdjdjfjfhdjsosoaodoj', (req, res) => {
+  const discordLink = process.env.dc;
+
+  res.writeHead(302, { Location: discordLink });
+  res.end();
+});
+
+app.get('*', function(req, res) {
+  res.status(404).redirect('/');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
