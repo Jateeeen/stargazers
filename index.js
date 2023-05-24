56c123b9e617df051fe51bdcb58d2599fc481481
Jateeeen <75193966+Jateeeen@users.noreply.github.com>
2023-05-24 03:32:01 +0000

    Added some minor changes

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('main.ejs');
